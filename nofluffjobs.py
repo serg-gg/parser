bc831c3d6cd4601f485badb8564c1c30090c4a28
Siarhei <siarheiburdzeika@gmail.com>
2023-05-22 16:33:21 +0200

    get vacancies list with links

--- nofluffjobs.py
+++ nofluffjobs.py
@@ -1,10 +1,12 @@
 import main
 
-nofluffjobs_request = main.get_request('https://nofluffjobs.com/pl/praca-zdalna?criteria=city%3Dwarszawa%20seniority%3Dtrainee&page=1')
+URL = 'https://nofluffjobs.com/pl/praca-zdalna?criteria=city%3Dwarszawa%20seniority%3Dtrainee&page=1'
+
+nofluffjobs_request = main.get_request(URL)
 nofluffjobs_soup = main.get_soup(nofluffjobs_request.content)
 
 
 paginator = nofluffjobs_soup.find('div', {'class': 'text-center mt-5 ng-star-inserted'})
 pages = paginator.find_all('a')
-for index in range(len(pages)-1):
-    print(f'page={(index+1)}')
+
+main.get_vacancies_links(main.get_vacancies_soup(pages, URL))
